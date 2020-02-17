fc07d54970f12aa4483ef14ada0babc52329ba4e
zero <mcheung@bisu.io>
2020-02-17 15:37:36 -0600

    Initial maven files

---
+++ backend/project-management-dashboard/src/main/java/com/project/management/dashboard/projectmanagementdashboard/ProjectManagementDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.project.management.dashboard.projectmanagementdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/project-management-dashboard/src/test/java/com/project/management/dashboard/projectmanagementdashboard/ProjectManagementDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.management.dashboard.projectmanagementdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
