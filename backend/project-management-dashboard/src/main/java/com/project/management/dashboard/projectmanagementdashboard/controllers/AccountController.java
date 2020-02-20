ccf0d3c52c9dbb452f2bc4ea6fb7409b70b289e2
zero <mcheung@bisu.io>
2020-02-19 19:01:08 -0600

    Added a getMapping to retrieve all users.

--- backend/project-management-dashboard/src/main/java/com/project/management/dashboard/projectmanagementdashboard/controllers/AccountController.java
+++ backend/project-management-dashboard/src/main/java/com/project/management/dashboard/projectmanagementdashboard/controllers/AccountController.java
@@ -1,4 +1,39 @@
 package com.project.management.dashboard.projectmanagementdashboard.controllers;
 
+import com.project.management.dashboard.projectmanagementdashboard.models.Account;
+import com.project.management.dashboard.projectmanagementdashboard.models.data.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials= "true", allowedHeaders = "*")
 public class AccountController {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @GetMapping("/users")
+    public List<Account> getAllUsers() {
+        return accountRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Void> proccessAddUser(@RequestBody Account account){
+        account.setPassword(BCrypt.hashpw(
+                account.getPassword(), BCrypt.gensalt(12)
+        ));
+
+        Account userAdded = accountRepository.save(account);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}/{username}").buildAndExpand(userAdded.getId(), userAdded.getUsername()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
 }
