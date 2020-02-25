77eb5912aec1e6b25b4b9d2c42a98bf7e1a051e3
zero <mcheung@bisu.io>
2020-02-24 20:00:46 -0600

    Reconfig the login to use Formik

--- frontend/project-management-dashboard/src/api/dashboard/DashboardDataService.js
+++ frontend/project-management-dashboard/src/api/dashboard/DashboardDataService.js
@@ -2,5 +2,15 @@ import axios from 'axios';
 import {API_URI} from '../../VariableProperties.js';
 
 class DashboardDataService {
-    
+    authenicateUser(id, usernameAuth, passwordAuth) {
+        return axios.get({
+            baseURL: `${API_URI}/users/${id}/basicauth`,
+            auth: {
+                username: usernameAuth,
+                password: passwordAuth
+            } 
+        })
+    }
 }
+
+export default new DashboardDataService();
--- frontend/project-management-dashboard/src/components/dashboard/LoginComponent.jsx
+++ frontend/project-management-dashboard/src/components/dashboard/LoginComponent.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AuthenicationService from "./AuthenicationService.js";
+import {Formik, ErrorMessage, Form, Field} from 'formik';
 
 class LoginComponent extends Component {
     constructor(props) {
@@ -19,17 +20,18 @@ class LoginComponent extends Component {
         this.setState({ [event.target.name]: event.target.value });
     }
     
-    handleSubmit(event) {
-        if(this.state.username==="test" && this.state.password==="test") {
-            AuthenicationService.successfulLogin(this.state.username, this.state.password);
-            this.props.history.push(`/users/${this.state.username}/dashboard`);
-            this.setState({loginFailed: false});
-            this.setState({loginSuccess: true})
-        } else {
-            this.setState({loginSuccess: false})
-            this.setState({loginFailed: true})
-            event.preventDefault();
+    handleSubmit(values) {
+        if(values.username==="test" && values.password==="test") {
+            AuthenicationService.successfulLogin(values.username, values.password);
+            this.props.history.push(`/users/${values.username}/dashboard`);
         }
+            // this.setState({loginFailed: false});
+            // this.setState({loginSuccess: true})
+        // } else {
+        //     this.setState({loginSuccess: false})
+        //     this.setState({loginFailed: true})
+        //     // event.preventDefault();
+        // }
     }
     
     closeNotification(event) {
@@ -37,62 +39,75 @@ class LoginComponent extends Component {
     }
 
     render() {
+        let {username, password} = this.state;
         return (
-            <section className="hero is-dark is-fullheight">
-                <div className="hero-body">
-                    <div className="container">
-                        <div className="column is-centered">
-                            <div className="column is-4 is-offset-4">
-                                <form onSubmit={this.handleSubmit} className="box">
-                                    <div className="field">
-                                        <label className="label has-text-weight-semibold has-text-justified is-size-5">
-                                            Username
-                                        </label>
-                                        <div className="control has-icons-left">
-                                            <input className="input"
-                                                type="text"
-                                                name = "username"
-                                                defaultValue={this.state.username} placeholder="e.g user@email.com"
-                                                required
-                                                onChange={this.handleChange} />
-                                            <span className="icon is-small is-left">
-                                                <i className="fas fa-envelope">
-                                                </i>
-                                            </span>
-                                            {this.state.usernameIsBlank && <p className="help is-danger">Please enter a valid password.</p>}
-                                        </div>
+            <Formik
+                initialValues={{username, password}}
+                validateOnBlur={true}
+                validateOnChange={true}
+                onSubmit={this.handleSubmit} 
+                onChange={this.handleChange}
+            >
+                {(props) => (
+                    <section className="hero is-dark is-fullheight">
+                        <div className="hero-body">
+                            <div className="container">
+                                <div className="column is-centered">
+                                    <div className="column is-4 is-offset-4">
+                                        <Form className="box">
+                                            <fieldset className="field">
+                                                <label className="label has-text-weight-semibold has-text-justified is-size-5">
+                                                    Username
+                                                </label>
+                                                <div className="control has-icons-left">
+                                                    <Field 
+                                                        className="input"
+                                                        type="text"
+                                                        name = "username"
+                                                        placeholder="e.g user@email.com"
+                                                        required 
+                                                    />
+                                                <span className="icon is-small is-left">
+                                                    <i className="fas fa-envelope">
+                                                    </i>
+                                                </span>
+                                                    <ErrorMessage name="username" component="p" className="help is-danger"/>
+                                                </div>
+                                            </fieldset>
+                                            <fieldset className="field">
+                                                <label className="label has-text-weight-semibold has-text-justified is-size-5">
+                                                    Password
+                                                </label>
+                                                <div className="control has-icons-left">
+                                                    <Field 
+                                                        className="input" 
+                                                        type="password"
+                                                        name="password"
+                                                        placeholder="*****"
+                                                        required 
+                                                    />
+                                                <span className="icon is-small is-left">
+                                                    <i className="fas fa-lock"></i>
+                                                </span>
+                                                    <ErrorMessage name="password" component="p" className="help is-danger" />
+                                                </div>
+                                            </fieldset>
+                                            <div className="field">
+                                                <p className="control">
+                                                    <button className="button is-success" type="submit">
+                                                        Login
+                                                    </button>
+                                                </p>
+                                            </div>
+                                            {this.state.loginFailed && <div className="notification is-centered is-danger"><button onClick={this.closeNotification} className="delete"></button>Invalid Username or Password</div>}
+                                        </Form>
                                     </div>
-                                    <div className="field">
-                                        <label className="label has-text-weight-semibold has-text-justified is-size-5">
-                                            Password
-                                        </label>
-                                        <div className="control has-icons-left">
-                                            <input className="input" 
-                                            type="password"
-                                            name="password"
-                                            defaultValue={this.state.password}
-                                            placeholder="*****"
-                                            required
-                                            onChange={this.handleChange} />
-                                        <span className="icon is-small is-left">
-                                            <i className="fas fa-lock"></i>
-                                        </span>
-                                        </div>
-                                    </div>
-                                    <div className="field">
-                                        <p className="control">
-                                            <button className="button is-success">
-                                                Login
-                                            </button>
-                                        </p>
-                                    </div>
-                                    {this.state.loginFailed && <div className="notification is-centered is-danger"><button onClick={this.closeNotification} className="delete"></button>Invalid Username or Password</div>}
-                                </form>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            </section>
+                    </section>
+                )}
+            </Formik>
         )
     }
 }
