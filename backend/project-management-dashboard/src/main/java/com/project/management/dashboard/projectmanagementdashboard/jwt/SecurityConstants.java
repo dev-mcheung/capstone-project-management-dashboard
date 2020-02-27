6bb2124c01802702fe2a01386ab22b75f7245534
zero <mcheung@bisu.io>
2020-02-27 04:25:47 -0600

    Modify the expiration time

--- backend/project-management-dashboard/src/main/java/com/project/management/dashboard/projectmanagementdashboard/jwt/SecurityConstants.java
+++ backend/project-management-dashboard/src/main/java/com/project/management/dashboard/projectmanagementdashboard/jwt/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.project.management.dashboard.projectmanagementdashboard.jwt;
 
 public class SecurityConstants {
     public static final String SECRET = System.getenv("JWT_SECRET");
-    public static final long EXPIRATION_TIME = 604_800_000; // 7 days
+    public static final long EXPIRATION_TIME = 604800000; // 7 days
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String LOGIN_URL = "/users/login";
