04a830c81d428288a14ba204b74727640330f297
zero <mcheung@bisu.io>
2020-03-04 09:11:15 -0600

    Rename DashboardApp.jsx to RouteHandler.jsx.

--- frontend/project-management-dashboard/src/App.js
+++ frontend/project-management-dashboard/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import DashboardApp from './components/dashboard/DashboardApp.jsx';
+import RouteHandler from './components/dashboard/RouteHandler.jsx';
 import 'bulma/css/bulma.min.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 
 function App() {
   return (
     <div className="App">
-      <DashboardApp />
+      <RouteHandler />
     </div>
   );
 }
